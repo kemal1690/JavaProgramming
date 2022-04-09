c8013f860f0bc699875c4f254064b44455633198
kml <fatih200530@hotmail.com>
2022-04-10 00:47:16 +0300

    week 12-13 review

--- LiveReviewSessions/week12/Telephone.java
+++ LiveReviewSessions/week12/Telephone.java
@@ -2,21 +2,32 @@ package week12;
 
 public class Telephone {
 
-  public  String number;
-  public static int quantity = 250;
-  public static double total = 15658.92;
-
-
-  public void setNumber(String number){
-
-
-  }
-
-}
-/*
-Write the definition of a class Telephone.
+   /*
+    Write the definition of a class Telephone.
 The class has no constructors, one instance variable of type String called number, and two public static variables.
 One is of type int called quantity, initialized to 250;
 the other is of type double called total, initialized to 15658.92.
 Create getNumber() and setNumber() methods that will manipulate with number instance variable.
- */
+     */
+
+  // no constructor means that we will use default construstor
+
+  public String number;
+
+  public static int quantity;
+
+  public static double total;
+
+  static {
+    quantity = 250;
+    total = 15658.92;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public String getNumber() {
+    return this.number;
+  }
+}
---
+++ LiveReviewSessions/week13/inheritance/EBook.java
@@ -0,0 +1,20 @@
+package week13.inheritance;
+
+public class EBook extends Book {
+
+    int size;
+    int pages;
+
+    public EBook(){
+        super();
+    }
+
+    public void readBook(){
+        System.out.println("Reading the book from my Tablet");
+        System.out.println("title = " + title);
+        System.out.println("author = " + author);
+        System.out.println("price = " + price);
+        System.out.println("size = " + size);
+        System.out.println("pages = " + pages);
+    }
+}
