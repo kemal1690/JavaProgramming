2116f6250ee036e8f8e258b66c067a47245b6950
kml <fatih200530@hotmail.com>
2022-04-19 23:23:54 +0300

    Custom Exceptions

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -1,32 +1,33 @@
 package day42_Exceptions;
 
-class FadyException extends RuntimeException{
+class FadyException extends RuntimeException{//IOT create custom unchecked exception we need to extend/inherit RuntimeException Class
 
-    public FadyException(){
+    //We have 2 constructors, so we have 2 ways to create an object
+
+    public FadyException(){//default constructor
         super("Time for a short break");
     }
 
     public FadyException(String message){
-        super(message);
+        super(message);// We are calling parent class constructor>>RuntimeException
     }
 
 }
 
 
-class NoBreakException extends Exception{ // custom checked exception
+class NoBreakException extends Exception{ // IOT create custom checked exception we need to extend/inherit Exception Class
 
 }
 
 public class CustomExceptions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-
-        // throw new FadyException("It's time for lunch break");
+       // throw new FadyException("It's time for lunch break");
 
 
         try {
-            throw new NoBreakException();
+            throw new NoBreakException();//checked exception
         } catch (NoBreakException e) {
             e.printStackTrace();
         }
@@ -35,4 +36,9 @@ public class CustomExceptions {
 
 
     }
+
+
+
+
 }
+
