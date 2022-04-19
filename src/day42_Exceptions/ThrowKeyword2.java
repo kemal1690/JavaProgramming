f284b321d51223dc89f59fcbfd565cef800f13a6
kml <fatih200530@hotmail.com>
2022-04-19 20:04:45 +0300

    Day 42 Exceptions

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,21 @@
+package day42_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+
+    public static void main(String[] args) {
+
+//throw keyword is used to create an exception manually
+        throw new NoSuchElementException("No such Element");
+        //    throw new InterruptedException(); checked exception
+
+
+        //  System.out.println("hello world");
+
+        // throw new Rectangle(5,7);
+
+
+
+    }
+}
